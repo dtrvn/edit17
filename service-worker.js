f9b79bcf00fbfc99ee7beb3ca7a6539a851529b3
dtrvn <truongsonoi121@gmail.com>
2026-09-04 10:41:41 +0700

    20260904_1041

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'qlct-pwa-v20260904-stock-mobile-detail-fit-a';
+const CACHE_NAME = 'qlct-pwa-v20260904-stock-mobile-detail-fit-b';
 const APP_SHELL = [
   './',
   './index.html',
